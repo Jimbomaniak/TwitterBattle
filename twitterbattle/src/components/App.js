bc169ed99674d5bbdd2feac93615ef351b7b128b
Olegka <jimboman54321@gmail.com>
2018-04-17 13:35:27 +0300

    add main component

---
+++ twitterbattle/src/components/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './App.css';
+import Popular from './Popular/Popular'
+
+class App extends Component {
+  render() {
+    return (
+      <div className="container">
+        <Popular/>
+      </div>
+    );
+  }
+}
+
+export default App;
