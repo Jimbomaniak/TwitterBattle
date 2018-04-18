4e9157a6d8cb9666528aabdeb03b2d95d19bb4fb
Olegka <jimboman54321@gmail.com>
2018-04-18 13:05:21 +0300

    dumb reusable component to show Player info

---
+++ twitterbattle/src/components/Battle/PlayerPreview/PlayerPreview.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+function PlayerPreview (props) {
+  return (
+    <div>
+      <div className='column'>
+        <img className='avatar'
+             src={props.avatar}
+             alt={`Avatar for ${props.username}`}
+        />
+        <h2 className='username'>{`@${props.username}`}</h2>
+      </div>
+      {props.children}
+    </div>
+  )
+}
+
+PlayerPreview.PropTypes = {
+  avatar: PropTypes.string.isRequired,
+  username: PropTypes.string.isRequired,
+};
+
+export default PlayerPreview
